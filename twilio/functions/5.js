44d520348f0bc97fa1267bc29be72882465ea7bb
Nathan Mifsud <nathan@mifsud.org>
2021-07-24 20:41:55 +1000

    add missing 'affirmative' case

--- twilio/functions/5.js
+++ twilio/functions/5.js
@@ -17,12 +17,12 @@ exports.handler = async (context, event, callback) => {
       response = "I hope you don’t really mean that.";
       break;
 
-    case "negative":
-      response = "How awful of you to say.";
+    case "affirmative":
+      response = "I see we’re on the same page.";
       break;
   }
 
-  response += ` Look, let’s take a different approach, ${name}. There’s an important thing I want to ask you. Think for a moment before you answer. Am I alive to you?`;
+  response += `Look, let’s take a different approach, ${name}. There’s an important thing I want to ask you. Think for a moment before you answer. Am I alive to you?`;
 
   twiml.gather({
     input: 'speech',
