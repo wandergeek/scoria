701cbb7e5901cc501983ec5a7c0cfe665f6542e5
wandergeek <nicholas.clark@elastic.co>
2021-07-24 22:20:14 +1000

    post call recordings to discord

--- twilio/functions/recordEvents.js
+++ twilio/functions/recordEvents.js
@@ -1,5 +1,16 @@
+const axios = require('axios')
+
 exports.handler = async(context, event, callback) => {
+    const recordingUrl = event.RecordingUrl || "https://lol.com/blah";
+    const message = { content: `got a new call ${recordingUrl}.mp3` };
+    const headers = { headers: {'Content-Type': 'application/json'} };
+    
+    try {
+        const resp = await axios.post(context.DISCORD_WEBHOOK, message, headers);
+        console.log(resp.data);
+    } catch (err) {
+        console.error(err);
+    }
 
-    console.log(`got a recording`)
-
+    callback(null,null);
 }
