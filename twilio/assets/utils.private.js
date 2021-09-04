527c1aa04f711935b55fd9a263ca4de0af15e28d
Nathan Mifsud <nathan@mifsud.org>
2021-09-04 22:54:24 +1000

    move checkSentiment to utils

--- twilio/assets/utils.private.js
+++ twilio/assets/utils.private.js
@@ -1,5 +1,23 @@
 module.exports = {
-    getRandomElement(array) {
-        return array[Math.floor(Math.random() * array.length)];
+  getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  },
+
+  //returns string "affirmative", "negative", or "neutral" depending on sentiment of input
+  checkSentiment(input) {
+    const threshold = 0.5; //sentiment ranked scale +-5
+    var Sentiment = require('sentiment');
+    var sentiment = new Sentiment();
+    var analysis = sentiment.analyze(input);
+  
+    if(analysis.comparative < -threshold) {
+    ret = "negative";
+    } else if(analysis.comparative >= -threshold && analysis.comparative < threshold) {
+    ret = "neutral";
+    } else if(analysis.comparative >= threshold ) {
+    ret = "affirmative";
     }
+  
+    return ret;
+  }
 }
